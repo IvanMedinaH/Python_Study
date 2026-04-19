403ed203ab7707b3499d5cde41e50beababd16ea
RealformStudio <sl4sh8@gmail.com>
2026-04-19 15:22:42 -0600

    repository configure, for python study

---
+++ 1_fundamentos/PythonSyntax.py
@@ -0,0 +1,9 @@
+"""In python blocks of code are delimited by spaces not {}
+    python uses 4 spaces
+"""
+
+nombre ="Juan perez"
+edad = 18
+peso = 24.50
+cartilla = True
+materias= [español, matematicas, biologia, quimica]
